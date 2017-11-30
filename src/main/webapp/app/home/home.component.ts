a10809da23c9aa191697015fb4ab6e973f4af58c
Jay Kola <jay@noesisinformatica.com>
2017-11-30 09:59:21 +0000

    feat: Added login redirect to followup actions page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
+import { Router } from '@angular/router';
 
 import { Account, LoginModalService, Principal } from '../shared';
 
@@ -19,7 +20,8 @@ export class HomeComponent implements OnInit {
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private router: Router
     ) {
     }
 
@@ -34,6 +36,8 @@ export class HomeComponent implements OnInit {
         this.eventManager.subscribe('authenticationSuccess', (message) => {
             this.principal.identity().then((account) => {
                 this.account = account;
+                // navigate user to followup-action page
+                this.router.navigate(['followup-action']);
             });
         });
     }
