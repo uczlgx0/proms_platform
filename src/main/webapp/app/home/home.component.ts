0debcbb80b98d209b2b5f36c8db8bf605eb60183
Jay Kola <jay@noesisinformatica.com>
2017-12-08 08:38:11 +0000

    feat: Logging in as consultant now redirects to procedure bookings

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 import { Router } from '@angular/router';
+import * as _ from 'underscore';
 
 import { Account, LoginModalService, Principal } from '../shared';
 
@@ -36,8 +37,15 @@ export class HomeComponent implements OnInit {
         this.eventManager.subscribe('authenticationSuccess', (message) => {
             this.principal.identity().then((account) => {
                 this.account = account;
-                // navigate user to followup-action page
-                this.router.navigate(['followup-action']);
+                console.log("this.account  = " , this.account );
+                if(_.contains(this.account.authorities, 'ROLE_CONSULTANT')) {
+                    // navigate user to followup-action page
+                    this.router.navigate(['procedure-booking']);
+                } else {
+                    // navigate user to followup-action page
+                    this.router.navigate(['followup-action']);
+                }
+
             });
         });
     }
