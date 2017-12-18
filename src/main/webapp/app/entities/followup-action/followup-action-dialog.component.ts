70b7fd44955439f02616493f1f7a5e4205d240ab
Jay Kola <jay@noesisinformatica.com>
2017-12-18 07:52:01 +0000

    feat: New follow up actions can only be created for patients with existing bookings

--- src/main/webapp/app/entities/followup-action/followup-action-dialog.component.ts
+++ src/main/webapp/app/entities/followup-action/followup-action-dialog.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Response } from '@angular/http';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable, Subscription } from 'rxjs/Rx';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { JhiEventManager, JhiAlertService, JhiParseLinks } from 'ng-jhipster';
 
 import { FollowupAction, ActionType } from './followup-action.model';
 import { FollowupActionPopupService } from './followup-action-popup.service';
@@ -21,7 +21,7 @@ import {IMyDpOptions} from 'mydatepicker';
     selector: 'jhi-followup-action-dialog',
     templateUrl: './followup-action-dialog.component.html'
 })
-export class FollowupActionDialogComponent implements OnInit {
+export class FollowupActionDialogComponent implements OnInit, OnDestroy {
 
     followupAction: FollowupAction;
     isSaving: boolean;
@@ -29,17 +29,23 @@ export class FollowupActionDialogComponent implements OnInit {
     questionnaireId: string;
     followupplans: FollowupPlan[];
     procedureBookings: any;
+    selectedProcedureBooking: ProcedureBooking;
+    selectedFollowupPlan: FollowupPlan;
+    bookingLinks: any;
+    bookingTotalItems: any;
+    bookingQueryCount: any;
+    eventSubscriber: Subscription;
     datePickerOptions: IMyDpOptions = {
         dateFormat: 'dd/mm/yyyy',
         minYear: 1850
     };
     patients: Patient[];
-
     questionnaires: Questionnaire[];
 
     constructor(
         public activeModal: NgbActiveModal,
         private jhiAlertService: JhiAlertService,
+        private parseLinks: JhiParseLinks,
         private followupActionService: FollowupActionService,
         private followupPlanService: FollowupPlanService,
         private patientService: PatientService,
@@ -106,18 +112,16 @@ export class FollowupActionDialogComponent implements OnInit {
     }
 
     onPatientSelected(option: IOption) {
+        // rest existing values
+        this.resetValues();
+        // now set patient id
         if(!this.followupAction.patient) {
             this.followupAction.patient = new Patient();
         }
         this.followupAction.patient.id = parseInt(option.value);
 
-        // now get procedure bookings for patient
-        this.procedureBookingService.findByPatientId(this.followupAction.patient.id,  {
-            page: 0,
-            size: 50,
-            sort: this.sort()}).subscribe(
-            (res: ResponseWrapper) => this.onBookingsSuccess(res.json, res.headers),
-            (res: ResponseWrapper) => this.onError(res.json))
+        // now get procedure bookings
+        this.loadBookings();
     }
 
     onQuestionnaireSelected(option: IOption) {
@@ -129,12 +133,65 @@ export class FollowupActionDialogComponent implements OnInit {
         this.followupAction.type = ActionType['QUESTIONNAIRE'];
     }
 
+    onBookingSelected(booking: ProcedureBooking) {
+        // first reset questionnaire related values
+        this.followupAction.questionnaire = null;
+        this.followupAction.name = null;
+        this.followupAction.type = null;
+        this.questionnaireId = null;
+
+        // now process booking and reset questionnaires
+        this.selectedProcedureBooking = booking;
+        this.selectedFollowupPlan = this.selectedProcedureBooking.followupPlan;
+        console.log("this.selectedFollowupPlan  = " , this.selectedFollowupPlan );
+        this.followupAction.followupPlan = this.selectedFollowupPlan;
+        // get questionnaires for procedure in booking
+        this.loadQuestionnaires(booking);
+    }
+
+    private loadBookings() {
+        // now get procedure bookings for patient
+        this.procedureBookingService.findByPatientId(this.followupAction.patient.id,  {
+            page: 0,
+            size: 50,
+            sort: this.sort()}).subscribe(
+            (res: ResponseWrapper) => this.onBookingsSuccess(res.json, res.headers),
+            (res: ResponseWrapper) => this.onError(res.json));
+        //this.formHeight = '500px';
+    }
+
+    private loadQuestionnaires(booking: ProcedureBooking) {
+        this.questionnaireService.questinnairesForProcedureLocalCode(booking.primaryProcedure).subscribe(
+            (res: ResponseWrapper) => this.questionnaires = res.json,
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+        // update form height
+        //this.formHeight = '750px';
+    }
+
     private onBookingsSuccess(data, headers) {
+        this.bookingLinks = this.parseLinks.parse(headers.get('link'));
+        this.bookingTotalItems = headers.get('X-Total-Count');
+        this.bookingQueryCount = this.bookingTotalItems;
         this.procedureBookings = data;
     }
 
     private sort() {
         return ['id,asc'];
+    }
+
+    private resetValues() {
+        this.selectedProcedureBooking = null;
+        this.selectedFollowupPlan = null;
+        this.followupAction = new FollowupAction();
+    }
+
+    private registerChangeInProcedureBookings() {
+        this.eventSubscriber = this.eventManager.subscribe('procedureBookingListModification', (response) => this.loadBookings());
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
     }
 }
 
