2db0064e40c077e446088eb0c1078909c9863ab3
Jay Kola <jay@noesisinformatica.com>
2018-03-16 07:38:24 +0000

    chore: Added license config etc

--- src/main/java/com/noesisinformatica/northumbriaproms/repository/search/AddressSearchRepository.java
+++ src/main/java/com/noesisinformatica/northumbriaproms/repository/search/AddressSearchRepository.java
@@ -1,5 +1,29 @@
 package com.noesisinformatica.northumbriaproms.repository.search;
 
+/*-
+ * #%L
+ * Proms Platform
+ * %%
+ * Copyright (C) 2017 - 2018 Termlex
+ * %%
+ * This software is Copyright and Intellectual Property of Termlex Inc Limited.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation as version 3 of the
+ * License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+ * #L%
+ */
+
 import com.noesisinformatica.northumbriaproms.domain.Address;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
